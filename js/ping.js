b85c75c082be11c4da73d7df61e6d599455bec83
Ibrahim Hassan <hassan.ibb120044@gmail.com>
2023-07-23 19:05:42 +0100

    Add files via upload

--- js/ping.js
+++ js/ping.js
@@ -1,13 +1,38 @@
-let myError = document.getElementById("error");
-console.log("myError");
-let myButton = document.getElementsByTagName("button");
-console.log("myButton");
-let myEmail = document.getElementsByTagName("input").value;
+let myError = document.querySelector("#error");
+let form = document.querySelector('form');
+const validMail = document.querySelector('#email-address');
 
-function errorMessage() {
-  console.log("button clicked");
-  myError.textContent = "Please provide a valid email address";
-}
+function myErrorMessage() {
+  if (validMail.value.trim() === '') {
+    let form = validMail.parentElement;
+    let errorMsg = form.querySelector('small');
+    errorMsg.style.display = 'block';
+    errorMsg.textContent = "field is empty";
+    form.classList.add('error');
+    form.classList.remove('success');
+  }
+   else {
+    let form = validMail.parentElement;
+    let errorMsg = form.querySelector('small');
+    errorMsg.style.display = 'none';
+    form.classList.remove('error');
+    form.classList.add('success');
+  }
+
+};
 
 
 
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  myErrorMessage()
+  console.dir(e)
+
+});
+
+// // email regress
+// function validAdd(validMail) {
+//   let EmailReg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]
+//   { 1, 3 } \.[0 - 9]{ 1, 3 } \.[0 - 9]{ 1, 3 }])| (([a - zA - Z\-0 - 9] +\.) +[a - zA - Z]{ 2,})) $ /
+//      return EmailReg.test(validMail);
+// };
